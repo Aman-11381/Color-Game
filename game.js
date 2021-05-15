2cb48085afb19e48e67db4bd25b39fc504a5a8b9
Aman Agarwal <83666330+Aman-11381@users.noreply.github.com>
2021-05-15 13:24:11 +0530

    add easy mode functionality and fix some bugs
    easy mode enables the user to play using only three tiles instead of the six tiles in default mode

--- game.js
+++ game.js
@@ -9,8 +9,6 @@ var index = 0;
 
 startOver();
 
-$("h1").html(generateColorName(colors[chosenColorIndex]).toUpperCase());
-
 // adding a click event listener to the whole web page
 $(document).on("click", function(event){
   // check to see if the area clicked is a correct tile or not
@@ -42,10 +40,56 @@ $(document).on("click", function(event){
 
 // adding a click event listener to the new colors button in the menu section
 $("#new-colors").click(function(){
-  // changing the text content to new colors
-  $(this).html("NEW COLORS");
+  // resetting the game
+  startOver();
+});
 
-  // adding the fade animations
+// adding a click event listener to the easy button
+$("#easy").click(function(){
+  // changing the number of colors to 3 so that the chosen number is from the first three numbers
+  numberOfColors = 3;
+
+  startOver();
+
+  //hiding the last three color tiles because the game mode changed to easy
+  $(".card-tile.hard-exclusive").each(function(){
+    // adding the hidden class to bottom three buttons to hide them
+    $(this).addClass("hidden");
+  });
+});
+
+// adding a click event listener to hard button
+$("#hard").click(function(){
+  // changing the number of colors back to 6
+  numberOfColors = 6;
+
+  startOver();
+
+  // making the hidden color tiles visible again
+  $(".card-tile.hard-exclusive").each(function(){
+    // removing the hidden class from bottom three buttons
+    $(this).removeClass("hidden");
+  });
+});
+
+// function to reset the page with new color values and a new chosen color
+function startOver() {
+  colors = [];
+  index=0;
+
+  chosenColorIndex = generateChosenIndex();
+
+  // adding the random color values in the colors list
+  for(var i=0; i<6; i++){
+    colors.push(generateColorValues());
+  }
+
+  $("h1").html(generateColorName(colors[chosenColorIndex]).toUpperCase());
+
+  // changing the text content of play again button to new colors
+  $("#new-colors").html("NEW COLORS");
+
+  // adding the fade animation to the title section bakcground color
   $(".title-section").css(
     "opacity", "0",
   );
@@ -58,34 +102,18 @@ $("#new-colors").click(function(){
     $(".title-section").css("background-color", "#21094e");
   }, 100);
 
-  $(".card-tile").each(function(){
-    $(this).css("opacity", "0");
-  });
-
-  // resetting the game
-  startOver();
-});
-
-// function to reset the page with new color values and a new chosen color
-function startOver() {
-  colors = [];
-  index=0;
-
-  chosenColorIndex = generateChosenIndex();
-
-  // adding the random color values in the colors list
-  for(var i=0; i<numberOfColors; i++){
-    colors.push(generateColorValues());
-  }
-
   // iterating through the color tiles one by one and adding the random generated background colors
   $(".card-tile").each(function() {
     var color = generateColorName(colors[index]);
 
     // adding the fade animations
+    $(".card-tile").each(function(){
+      $(this).css("opacity", "0");
+    });
     $(this).animate({
       opacity: "1",
     }, "slow");
+
     $(this).css("background-color", color);
     index++;
   });
@@ -98,9 +126,9 @@ function generateChosenIndex() {
 
 // function to create three random values and store them in an array to make rgb colors
 function generateColorValues() {
-  var red = Math.floor(Math.random()*255+1);
-  var green = Math.floor(Math.random()*255+1);
-  var blue = Math.floor(Math.random()*255+1);
+  var red = Math.floor(Math.random()*256);
+  var green = Math.floor(Math.random()*256);
+  var blue = Math.floor(Math.random()*256);
 
   return [red, green, blue];
 }
